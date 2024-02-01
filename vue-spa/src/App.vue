1bc369b2268c7646f63561db805fb46f630d337e
Panagiotis <panajotbilimani@gmail.com>
2024-02-01 20:30:54 +0200

    commit

--- vue-spa/src/App.vue
+++ vue-spa/src/App.vue
@@ -1,22 +1,350 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+ <div id="app"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Free Book Me In Scotty</title>
+
+
+   
+
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+       <style> 
+
+
+   
+
+    </style>
+
+</head>
+
+<body>
+    <header>
+       <nav class="navbar navbar-expand-sm fixed top  ">
+        <div class="container">
+  <a class="navbar-brand" href="#" style="color: white;">
+    <i class="fas fa-calendar fa-2x"></i> Book Me In Scotty
+    <p>Host & Share Events!</p>
+</a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#" style="color:white;"><i class="fas  fa-fingerprint"></i> Login <span class="sr-only">(current)</span></a>
+      </li>
+       <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" style="color:white;" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-globe"></i> English
+        </a>
+         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+    </li>
+       
+   <li class="nav-item">
+       <button type="button" class="btn btn-danger btn-lg"><i class="fas fa-calendar-day"></i> Browse Events</button>
+      </li>
+     
+       <li class="nav-item">
+    <button type="button" class="btn btn-light">
+    <i class="fa-solid fa-mobile fa-1x"></i>
+</button>
+      </li>
     </ul>
   </div>
+</nav>
+    </header>
+
+     <main>
+   <div id="main-image" class="jumbotron">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 btn-holder ">
+            </div>
+            <div class="col-sm-6">
+                <h1 class="text-left" style="color: orange; font-weight: bold;">Book Me In Scotty</h1>
+                <h1 class="text-left" style="color: white; font-weight: bold;">Organize, Host and Share your Events</h1>
+                <br>
+
+                <a href="#" class="btn btn-light btn-lg active" role="button" aria-pressed="true">
+                    <i class="fas fa-cloud-download-alt"></i> For Individuals
+                </a>
+
+                <a href="#" class="btn btn-success btn-lg active" role="button" aria-pressed="true">
+                    <i class="fas fa-shopping-cart"></i> For Business
+                </a>
+
+                <a href="#" class="btn btn-light btn-lg active" role="button" aria-pressed="true">
+                    <i class="fas fa-book"></i> Docs
+                </a>
+               </div>
+               </div>
+              </div>
+              </div> 
+
+ 
+ <br>
+ <br>
+ <br>
+         <div id="upcoming-events" class="jumbotron">
+        <h1 class="text-center">  <i class="fas fa-hourglass-half"></i> Upcoming Events </h1>
+        <br>
+        <br>
+        <div class="container ">  
+        <div class="row ">
+        <div class="col">
+       <div class="card-deck">
+      <div class="card  mb-6 position-relative" >
+      <img src="image4.jpg" class="card-img-top" height="300">
+          <span class="badge badge-dark position-absolute top-0 end-0" style="margin-top: 10px; margin-left:280px;">Upcoming</span>
+          <span class="badge badge-danger position-absolute top-0 end-0" style="margin-top: 29px; margin-left:290px;"> 223days</span>
+         <span class="badge badge-primary mr-auto" style="margin-top:-18px;">Free</span>
+
+            <div class="card-body">
+       <p style="font-size: smaller; color: blue; display: flex; justify-content: left;">
+        <span style="padding-right: 5px;">Oct 1 ,</span>
+        <span style="border-right: 1px solid blue; margin-right :5px ; padding-right: 5px;">2020</span>
+        <span style="padding-right: 5px;">Dec 31 ,</span>
+       <span style="border-right: 1px solid blue;  margin-right :5x ; padding-right: 5px;">2020</span>
+       <span style="padding-right: 5px;">Coomalbidgup</span>
+</p>
+      <h5 class="card-title">Global Marketing Conference</h5>
+      <p class="card-text">Wheelbarrow Well.</p>
+    </div>
+     <div class="card-footer text-center" style="padding:2px;">
+        <p style="margin-top:7px;"> Business&Seminars </p>
+         </div>
+          </div>
+
+  <div class="card mb-7" >
+    <img src="image1.jpg" class="card-img-top" height="300">
+              <span class="badge badge-dark position-absolute top-0 end-0" style="margin-top: 10px; margin-left:280px;">Upcoming</span>
+              <span class="badge badge-danger position-absolute top-0 end-0" style="margin-top: 29px; margin-left:290px;"> 223days</span>
+                <span class="badge badge-primary mr-auto" style="margin-top:-18px;">Free</span>
+
+    <div class="card-body">
+        <p style="font-size: smaller; color:blue; display: flex; justify-content: left;">
+         <span style="padding-right: 5px;">Oct 1 ,</span>    
+         <span style="border-right: 1px solid blue; margin-right :5px ; padding-right: 5px;">2020</span>
+         <span style="padding-right: 5px;"> Dec 31 ,</span>
+         <span style="border-right: 1px solid blue;  margin-right :5x ; padding-right: 5px;">2020</span>
+         <span style="padding-right: 5px;"> Riverview </span>
+</p>
+      <h5 class="card-title">Caribbean Karaoke NIghts </h5>
+        <p class="card-text">Bridgeadale.</p>
+    </div>
+    <div class="card-footer text-center" style="padding:2px;" >
+        <p style="margin-top:7px;">Music & Concerts </p>
+    </div>
+  </div>
+
+  <div class="card mb-8">
+    <img src="image5.jpg" class="card-img-top" height="300">
+              <span class="badge badge-dark position-absolute top-0 end-0" style="margin-top: 10px; margin-left:280px;">Upcoming</span>
+             <span class="badge badge-danger position-absolute top-0 end-0" style="margin-top: 29px; margin-left:290px;"> 223days</span>
+             <span class="badge badge-primary mr-auto" style="margin-top:-18px;">Free</span>
+
+    <div class="card-body">
+        <p style="font-size: smaller; color: blue; display: flex; justify-content: left;">
+        <span style="padding-right: 5px;"> Oct 1 ,</span>
+        <span style="border-right: 1px solid blue; margin-right :5px ; padding-right: 5px;">2020</span>
+        <span style="padding-right: 5px;"> Dec 31 ,</span> 
+        <span style="border-right: 1px solid blue;  margin-right :5x ; padding-right: 5px;">2020</span>
+        <span style="padding-right: 5px;"> Lanzhou </span>
+</p>
+    <h5 class="card-title">VR assistant Workshop</h5>
+    <p class="card-text"> Roller Rink.</p>
+ </div>
+ <div class="card-footer text-center" style="padding:2px;">
+     <p style="margin-top:7px"> Business & Seminars</p>
+ </div>
+</div>
+    </div>
+    </div>
+</div>   
+
+      <br>
+      <br>
+          <div class="text-center">
+            <a href="#" class="btn btn-danger btn-lg active" role="button" aria-pressed="true"> <i class="fas fa-calendar-day"></i> View All Events </a>
+            </div>
+ </div>
+</div>
+ <br>
+ <br>
+ <br>    
+          <div id="event-category" class="jumbotron">
+
+           <h1 class="text-center"> Event Categories </h1>
+           <br>
+           <br>
+                <div class="container">
+  <div class="row">
+    <div class="col-6 col-md-3">
+          <div class="card border-primary mb-3" style="max-height: 8rem;">
+             <div class="card-img" style="background: url('image9.jpg'); background-size: cover; height: 8rem;"></div>
+        <div class="card-body text-white" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 1; display: inline-block;">
+          <h5 class="card-title">Business & Seminars</h5>
+    </div>
+      </div>
+      </div>
+    <div class="col-6 col-md-3">
+      <div class="card border-secondary mb-3" style="max-height: 8rem;">
+        <div class="card-img" style="background: url('image10.jpg'); background-size: cover; height: 8rem;"></div>
+        <div class="card-body text-white" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 1; display: inline-block;">
+          <h5 class="card-title">Sports & Fitness</h5>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-6 col-md-3">
+      <div class="card border-success mb-3" style="max-height: 8rem;">
+         <div class="card-img" style="background: url('image2.jpg'); background-size: cover; height: 8rem;"></div>
+        <div class="card-body text-white" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 1; display: inline-block;">
+          <h5 class="card-title">Music & Concerts</h5>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-6 col-md-3">
+      <div class="card border-danger mb-3" style="max-height: 8rem;">
+      <div class="card-img" style="background: url('image11.jpg'); background-size: cover; height: 8rem;"></div>
+    <div class="card-body text-white" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 1; display: inline-block;">          
+        <h5 class="card-title">Food & Drink</h5>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+ <br>
+ <br>
+
+                  <h6 class="text-center">How-To Host Your Event </h6>
+                   <h1 class="text-center"> Event management life-cycle</h1>
+                   <br>
+                   <br>
+                <div class="d-flex justify-content-center">
+  <ul class="list-group list-group-horizontal-lg justify-content-center list-group-flush">
+    <li class="list-group-item text-center"> 
+      <i class="far fa-calendar-alt fa-6x"></i>
+      <br>
+      <br>
+      <h1>1. Plan the event </h1> 
+      <p>from idea to action plan </p>
+    </li>
+    <li class="list-group-item text-center">  
+      <i class="fas fa-ticket-alt fa-6x"></i> 
+      <br>
+      <br>                       
+      <h1>2. Organize the event  </h1>
+      <p>From action plan to execution </p>
+    </li>
+    <li class="list-group-item text-center">  
+      <i class="fas fa-walking fa-6x"> </i>
+      <br>
+      <br>
+      <h1>3. Promote the event  </h1>
+      <p> Spreading the word and selling tickets</p>
+    </li>
+    <li class="list-group-item text-center"> 
+      <i class="fas fa-photo-video fa-6x"></i>
+      <br>
+      <br>
+      <h1>4. Host the event </h1> 
+      <p> Host your event and collect feedback</p>
+    </li>
+  </ul>
+</div>
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+  <footer class="bg-dark text-center text-white" style="margin-top: -20px; padding-top: 10px;">
+  <div class="container">
+    <table class="table table-borderless table-dark mx-auto" style="max-width: 1200px;">
+        <thead>
+            <tr>
+                <th scope="col"></th> <!-- Moved to the left -->
+                <th scope="col">Useful Links</th>
+                <th scope="col">Contact</th>
+                <th scope="col">Social Connections</th>
+            </tr>
+        </thead>
+        <tbody>
+                <tr>
+                <td class="text-center">
+                <i class="fas  fa-calendar fa-5x " style="vertical-align: top;"></i> 
+                <br>
+                <br>                   
+                <p>Book me in Scotty</p>
+                   <p>Host & Share Events!</p>
+                </td>
+                <td class="text-center">
+                    <p>Events</p>
+                    <p>About</p>
+                    <p>Term & Conditions</p>
+                    <p>Privacy Policy</p>
+                </td>
+                <td class="text-center">
+                    <p style="color: blue;">Send us a Message</p>
+                    <p>
+                        85 Golden Street, Darlinghurst<br>
+                        ERP 2019, United States
+                    </p>
+                    <p><i class="fas fa-envelope"></i> info@bmis.com</p>
+                    <p><i class="fas fa-mobile-alt"></i> 1800-180-0808</p>
+                </td>
+                <td class="text-center">
+                    <p>Find us on social platforms</p>
+                    <a href="#" class="social-icon"><i class="fab fa-facebook fa-2x"></i></a>
+                    <a href="#" class="social-icon"><i class="fab fa-twitter fa-2x"></i></a>
+                    <a href="#" class="social-icon"><i class="fab fa-instagram fa-2x"></i></a>
+                    <a href="#" class="social-icon"><i class="fab fa-linkedin fa-2x"></i></a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+
+   
+
+ <p class="bg-dark text-center text-white">
+    <span style="color: blue;">&copy;</span> 2020 
+    <span style="color: blue;">BMIS</span>. Product by 
+    <span style="color: blue;">eView Group</span>
+</p>
+
+
+ </footer>
+
+
+</body>
+</html>
+
+
+</div> 
 </template>
 
 <script>
@@ -31,30 +359,60 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.17.1/font/bootstrap-icons.css" rel="stylesheet"> 
+    <!--load all Font Awesome styles -->
+    <link href="css/all.css" rel="stylesheet">
+    #main-image
+      {
+        background:url('image2.jpg') top center;
+        background-size: cover;
+        margin-top:0em;
+        padding-top: 20em;
+        padding-bottom:20em;
+        position: relative;
+        color: white;
+        }
+
+        .nav-item {
+          margin-right: 10px; /* Adjust the value according to your preference */
+            }
+
+header {
+     background-size: cover;
+       background-image:url('image2.jpg');
 }
 
-h1, h2 {
-  font-weight: normal;
+.hidden-spacer {
+    height: 56px
 }
+         
+   #upcoming-events
+   {
+    background:url(image7.jpg);
+    background-size: cover;
+        margin-top:-10em;
+        padding-top: 5em;
+        padding-bottom:5em;
+        position: relative;
+   }
+   #event-category
+    {
+        background:url(image8.jpg);
+        background-size: cover;
+        margin-top:-10em;
+        padding-top: 5em;
+        padding-bottom:5em;
+        position: relative;
+   }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+   #business
+    {
+    background:url(image9.jpg);
+    background-size: cover;
+        margin-top:-10em;
+        padding-top: 5em;
+        padding-bottom:5em;
+        position: relative;
+   }
 </style>
